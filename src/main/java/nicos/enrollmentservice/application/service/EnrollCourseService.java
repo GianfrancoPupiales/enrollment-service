834796121d9dbbbd2a39c9656c8ee558790478ad
Gianfranco Pupiales <gianfranco.pupiales@epn.edu.ec>
2026-09-23 19:54:55 -0500

    feat: implementar el contrato

---
+++ src/main/java/nicos/enrollmentservice/application/service/EnrollCourseService.java
@@ -0,0 +1,27 @@
+package nicos.enrollmentservice.application.service;
+
+import nicos.enrollmentservice.application.port.in.EnrollCourseCommand;
+import nicos.enrollmentservice.application.port.in.EnrollCourseUseCase;
+import nicos.enrollmentservice.domain.Enrollment;
+import nicos.enrollmentservice.domain.EnrollmentId;
+import nicos.enrollmentservice.domain.port.out.EnrollmentRepository;
+
+import java.util.Optional;
+
+public class EnrollCourseService implements EnrollCourseUseCase {
+    private final EnrollmentRepository enrollmentRepository;
+
+    public EnrollCourseService(EnrollmentRepository enrollmentRepository) {
+        this.enrollmentRepository = enrollmentRepository;
+    }
+
+    @Override
+    public EnrollmentId enroll(EnrollCourseCommand command) {
+        Optional<Enrollment> enrollmentOpt = enrollmentRepository.findByStudentIdAndAcademicPeriodId(command.studentId(), command.academicPeriodId());
+        Enrollment enrollment = enrollmentOpt.orElseGet(() -> Enrollment.create(command.studentId(), command.academicPeriodId()));
+        enrollment.enrollCourse(command.courseId(), command.credits());
+        enrollmentRepository.save(enrollment);
+
+        return enrollment.getEnrollmentId();
+    }
+}
