34c7fc1709d36e000aeeda9aacdf9b0a029eb92c
Gianfranco Pupiales <gianfranco.pupiales@epn.edu.ec>
2026-09-24 23:03:15 -0500

    feat: definir entidad en la capa de infraestructura

---
+++ src/main/java/nicos/enrollmentservice/infraestructure/adapter/out/persistence/EnrollmentJpaEntity.java
@@ -0,0 +1,24 @@
+package nicos.enrollmentservice.infraestructure.adapter.out.persistence;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table(name = "enrollment")
+public class EnrollmentJpaEntity {
+    @Id
+    @Column(name = "enrollment_id")
+    private UUID id;
+    @Column(name = "student_id", nullable = false)
+    private String studentId;
+    @Column(name = "academic_period_id", nullable = false)
+    private String academicPeriodId;
+
+    @Column(name = "max_allowed_credits", nullable = false)
+    private int maxAllowedCredits;
+
+    @OneToMany(mappedBy = "enrollment")
+    private List<EnrolledCourseJpaEntity> courses;
+}
