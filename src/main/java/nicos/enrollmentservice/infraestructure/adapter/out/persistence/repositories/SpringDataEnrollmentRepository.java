c3957ccfa2f793d22530d5c4ac9c91aa8fcf2d61
Gianfranco Pupiales <gianfranco.pupiales@epn.edu.ec>
2026-09-25 21:44:54 -0500

    feat: añadir firma para obtener el EnrollmentJpaEntity

--- src/main/java/nicos/enrollmentservice/infraestructure/adapter/out/persistence/repositories/SpringDataEnrollmentRepository.java
+++ src/main/java/nicos/enrollmentservice/infraestructure/adapter/out/persistence/repositories/SpringDataEnrollmentRepository.java
@@ -3,7 +3,9 @@ package nicos.enrollmentservice.infraestructure.adapter.out.persistence.reposito
 import nicos.enrollmentservice.infraestructure.adapter.out.persistence.entities.EnrollmentJpaEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface SpringDataEnrollmentRepository extends JpaRepository<EnrollmentJpaEntity, UUID> {
+    Optional<EnrollmentJpaEntity> findByStudentIdAndAcademicPeriodId(String studentId, String academicPeriodId);
 }
