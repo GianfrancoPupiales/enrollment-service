4098c9a04c9fc2199bb800978e4d1013ba6f2c7b
Gianfranco Pupiales <gianfranco.pupiales@epn.edu.ec>
2026-09-25 20:36:24 -0500

    feat: Crear mapper

---
+++ src/main/java/nicos/enrollmentservice/infraestructure/adapter/out/persistence/mapper/EnrollmentMapper.java
@@ -0,0 +1,10 @@
+package nicos.enrollmentservice.infraestructure.adapter.out.persistence.mapper;
+
+import nicos.enrollmentservice.domain.*;
+import nicos.enrollmentservice.infraestructure.adapter.out.persistence.EnrollmentJpaEntity;
+
+import java.util.List;
+
+public class EnrollmentMapper {
+    
+}
